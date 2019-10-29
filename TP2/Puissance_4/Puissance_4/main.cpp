348be68a6abda23530a0917a1de82020ca4b699b
AntoinePlouffe <antoineplouffe0602@gmail.com>
2019-10-29 16:20:46 -0400

    Cours

---
+++ TP2/Puissance_4/Puissance_4/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int nbr1 = 5;
+
+int main()
+{
+	cout << "  0 1 2 3 4 5 6" << endl;
+	char grid[6][7];
+	for (int r = 0; r < 6; r++)
+	{
+		cout << nbr1 <<"|";
+		nbr1--;
+
+		for (int c = 0; c < 7; c++)
+		{
+			grid[r][c] = 0;
+			cout << grid[r][c] << "|";
+		}
+		cout << endl;
+	}
+
+	system("pause");
+}
+
